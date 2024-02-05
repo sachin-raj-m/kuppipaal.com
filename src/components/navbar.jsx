98223dca665d6a635a70df1785c86d626f24fe82
sachin-raj-m <sachinraj2323@gmail.com>
2024-02-05 23:46:55 +0530

    fix : navbar css updare

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
+import styles from './navbar.module.css';
 
 const Navbar = () => {
   return (
